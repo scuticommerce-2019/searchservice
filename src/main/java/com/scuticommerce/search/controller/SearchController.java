b1c3d9e9a4ea2a649af3f80cd6df075c961a55c3
Kapil Kataria <kapil.kataria@kp.org>
2019-03-09 22:18:24 -0800

    adding elastic search features.

--- src/main/java/com/scuticommerce/search/controller/SearchController.java
+++ src/main/java/com/scuticommerce/search/controller/SearchController.java
@@ -1,18 +1,27 @@
 package com.scuticommerce.search.controller;
 
 import com.scuticommerce.search.model.SearchData;
+import com.scuticommerce.search.service.ElasticService;
 import com.scuticommerce.search.service.RedisSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping(value = "/api/search/")
 public class SearchController {
 
+    private static final String PRODUCT = "product";
+    private static final String ORDER = "order";
+
     @Autowired
     RedisSearchService service;
+
+    @Autowired
+    ElasticService elasticService;
 
     @GetMapping("/{key}")
     public ResponseEntity<?> search(@PathVariable String key){
@@ -29,6 +38,32 @@ public class SearchController {
         data.setScore(0.2);
 
         return new ResponseEntity<>(service.createSuggestions (data), HttpStatus.OK);
+    }
+
+    @PostMapping("/writeindex")
+    public ResponseEntity<?> writeIndex(@RequestParam("indexName") String name, @RequestParam("type") String type,
+                                        @RequestParam("id") String id, @RequestBody String payload){
+
+        return new ResponseEntity<>(elasticService.writeDocument(name, type,id,payload), HttpStatus.OK);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> writeIndex(@RequestParam("indexName") String name, @RequestParam("doc") String type,
+                                        @RequestParam("id") String id) throws IOException {
+
+        return new ResponseEntity<>(elasticService.getRequest(name, type,id), HttpStatus.OK);
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<?> searchProduct() throws IOException {
+
+        return new ResponseEntity<>(elasticService.search(PRODUCT), HttpStatus.OK);
+    }
+
+    @GetMapping("/order")
+    public ResponseEntity<?> searchOrder() throws IOException {
+
+        return new ResponseEntity<>(elasticService.search(ORDER), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/scuticommerce/search/service/ElasticService.java
@@ -0,0 +1,160 @@
+package com.scuticommerce.search.service;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.replication.ReplicationResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ElasticService {
+
+    /**
+     * call client.close(); when done
+     * @return
+     */
+    public RestHighLevelClient getClient(){
+
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+
+        return client;
+
+    }
+
+    public IndexRequest createJsonRequest(String index, String type, String id, String payload) {
+
+        IndexRequest request = new IndexRequest(index, type, id);
+
+        return request.source(payload, XContentType.JSON);
+
+    }
+
+    public String writeDocument(String index, String type, String id, String payload){
+
+        IndexRequest request = createJsonRequest( index,  type,  id,  payload);
+
+        try {
+
+            IndexResponse indexResponse = getClient().index(request, RequestOptions.DEFAULT);
+
+            System.out.println(indexResponse.toString());
+
+            return indexResponse.getResult().toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "OK";
+
+    }
+
+    public GetResponse getRequest(String index, String type, String id) throws IOException {
+
+        GetRequest getRequest = new GetRequest(index,type,id);
+
+        GetResponse response = getClient().get(getRequest, RequestOptions.DEFAULT);
+
+        return response;
+    }
+
+    public GetResponse getAysncRequest(String index, String type, String id) throws IOException {
+
+        GetRequest request = new GetRequest(index,type,id);
+
+        // TODO: 2019-03-09 check aysnc
+        GetResponse response = null;//getClient().getAsync(request, RequestOptions.DEFAULT, listner);
+
+        return response;
+    }
+
+    ActionListener<Boolean> listener = new ActionListener<Boolean>() {
+        @Override
+        public void onResponse(Boolean exists) {
+
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+
+        }
+    };
+
+
+    public DeleteResponse deleteRequest(String index, String type, String id) throws IOException {
+
+        DeleteRequest deleteRequest = new DeleteRequest(index,type,id);
+
+        DeleteResponse response = getClient().delete(deleteRequest, RequestOptions.DEFAULT);
+
+        return response;
+    }
+
+    public UpdateResponse updateRequest(String index, String type, String id) throws IOException {
+
+        UpdateRequest request = new UpdateRequest(index,type,id);
+
+        UpdateResponse response = getClient().update(request, RequestOptions.DEFAULT);
+
+        return response;
+    }
+
+    public SearchResponse search(String type) throws IOException {
+
+        //pass index in constructor or it will search all indexes
+        SearchRequest searchRequest = new SearchRequest(type);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+
+        SearchResponse response = getClient().search(searchRequest, RequestOptions.DEFAULT);
+
+        return response;
+
+    }
+
+    private void debugResponse(IndexResponse indexResponse){
+
+        String index = indexResponse.getIndex();
+        String type = indexResponse.getType();
+        String id = indexResponse.getId();
+        long version = indexResponse.getVersion();
+        if (indexResponse.getResult() == DocWriteResponse.Result.CREATED) {
+
+        } else if (indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
+
+        }
+        ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
+        if (shardInfo.getTotal() != shardInfo.getSuccessful()) {
+
+        }
+        if (shardInfo.getFailed() > 0) {
+            for (ReplicationResponse.ShardInfo.Failure failure :
+                    shardInfo.getFailures()) {
+                String reason = failure.reason();
+            }
+        }
+    }
+
+}
