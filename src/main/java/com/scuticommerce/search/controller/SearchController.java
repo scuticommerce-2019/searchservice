304512aad10c909ab5a8014a9032d26db9494d14
Kapil Kataria <kapil.kataria@kp.org>
2019-03-16 00:49:12 -0700

    updates for common module.

--- src/main/java/com/scuticommerce/search/controller/SearchController.java
+++ src/main/java/com/scuticommerce/search/controller/SearchController.java
@@ -2,7 +2,6 @@ package com.scuticommerce.search.controller;
 
 import com.scuticommerce.search.model.SearchData;
 import com.scuticommerce.search.service.ElasticService;
-import com.scuticommerce.search.service.RedisSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +18,10 @@ public class SearchController {
     private static final String PRODUCT = "product";
     private static final String ORDER = "order";
 
-    @Autowired
-    RedisSearchService service;
 
     @Autowired
     ElasticService elasticService;
 
-    @GetMapping("/{key}")
-    public ResponseEntity<?> search(@PathVariable String key){
-
-        return new ResponseEntity<>(service.getSuggestions(key), HttpStatus.OK);
-    }
-
-    @PostMapping("/createindex")
-    public ResponseEntity<?> createIndex(@RequestParam("name") String name){
-
-        SearchData data  = new SearchData();
-        data.setKeyword(name);
-        data.setPayload(name);
-        data.setScore(0.2);
-
-        return new ResponseEntity<>(service.createSuggestions (data), HttpStatus.OK);
-    }
 
     @PostMapping("/writeindex")
     public ResponseEntity<?> writeIndex(@RequestParam("indexName") String name, @RequestParam("type") String type,
